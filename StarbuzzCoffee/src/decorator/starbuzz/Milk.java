400d312512f77b94cb9eb46b66db7ab8cf212427
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-05-29 22:10:31 +0200

    added Milk Condiment

---
+++ StarbuzzCoffee/src/decorator/starbuzz/Milk.java
@@ -0,0 +1,23 @@
+package decorator.starbuzz;
+
+public class Milk extends CondimentDecorator {
+	
+	Beverage beverage;
+
+	public Milk(Beverage beverage) {
+		this.beverage = beverage;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return beverage.getDescription() + ", Milk";
+	}
+
+	@Override
+	public double cost() {
+		// TODO Auto-generated method stub
+		return beverage.cost() + 0.1;
+	}
+
+}
