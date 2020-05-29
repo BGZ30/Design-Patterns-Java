51914cd8b521625c3527ef2506c7f83e135959ee
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-05-29 21:57:09 +0200

    New coffees types

---
+++ StarbuzzCoffee/src/decorator/starbuzz/Espresso.java
@@ -0,0 +1,13 @@
+package decorator.starbuzz;
+
+public class Espresso extends Beverage{
+
+	public Espresso() {
+		description = "Espresso Coffee";
+	}
+	
+	public double cost() {
+			return 1.99;
+	}
+
+}
