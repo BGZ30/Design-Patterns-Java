3b69215f7a439be46d93b995b8b11c35b486b7d8
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-06-02 21:02:22 +0200

    fixed the constructor

--- Command_Pattern/src/homeAutomation/Devices/CeilingFan/CeilingFanOff.java
+++ Command_Pattern/src/homeAutomation/Devices/CeilingFan/CeilingFanOff.java
@@ -5,11 +5,9 @@ import homeAutomation.RemoteControl.Command;
 public class CeilingFanOff implements Command{
 
 	CeilingFan fan;
-	String mode;
 	
-	public CeilingFanOff(CeilingFan fan, String mode) {
+	public CeilingFanOff(CeilingFan fan) {
 		this.fan = fan;
-		this.mode = mode;
 	}
 	
 	public void excute() {
