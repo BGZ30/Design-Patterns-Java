cc7419ad11cbfb2f5a27d6a21f2a6cc201167db4
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-06-06 17:03:18 +0200

    Test code - print the menus

---
+++ Iterator_Pattern/twoMenusSimple/src/twoMenusSimple/People/ClientForTest.java
@@ -0,0 +1,15 @@
+package twoMenusSimple.People;
+
+import twoMenusSimple.Menus.*;
+
+public class ClientForTest {
+
+	public static void main(String args[]) {
+		PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+		DinerMenu dinerMenu = new DinerMenu();
+		
+		Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu);
+		
+		waitress.printMenu();
+	}
+}
