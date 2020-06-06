711cd8dee71454b21390b794ad1d0f89cc86d827
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-06-06 16:33:46 +0200

    added menuItem class

---
+++ Iterator_Pattern/twoMenusSimple/src/twoMenusSimple/Menus/MenuItem.java
@@ -0,0 +1,34 @@
+package twoMenusSimple.Menus;
+
+public class MenuItem {
+
+	String name;
+	String description;
+	boolean vegetarian;
+	double price;
+	private boolean vegeterian;
+	
+	public MenuItem(String name, String description, boolean vegeterian, double price) {
+		this.name = name;
+		this.description = description;
+		this.vegetarian = vegeterian;
+		this.price = price;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String description() {
+		return description;
+	}
+	
+	public boolean isVegeterian() {
+		return vegeterian;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+
+}
