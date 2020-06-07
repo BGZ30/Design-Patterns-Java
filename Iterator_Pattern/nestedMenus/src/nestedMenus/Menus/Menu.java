e96a18e3b5b44b569eee1778290089d16c8441b2
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-06-07 19:05:13 +0200

    menu class

---
+++ Iterator_Pattern/nestedMenus/src/nestedMenus/Menus/Menu.java
@@ -0,0 +1,50 @@
+package nestedMenus.Menus;
+
+import java.util.Iterator;
+import java.util.ArrayList;
+
+public class Menu extends MenuComponent {
+
+	ArrayList<MenuComponent> menu = new ArrayList<MenuComponent>();
+	String name;
+	String description;
+	
+	public Menu(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+
+	public void add(MenuComponent component) {
+		menu.add(component);
+	}
+	
+	public void remove(MenuComponent component) {
+		menu.remove(component);
+	}
+	
+	public MenuComponent getChild(int i) {
+		return (MenuComponent)menu.get(i);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String description() {
+		return description;
+	}
+
+	public void print() {
+		
+		System.out.print("\n" + getName());
+		System.out.println(", " + getDescription());
+		System.out.println("---------------------");
+	
+		Iterator<MenuComponent> iterator = menu.iterator();
+		
+		while(iterator.hasNext()) {
+			MenuComponent component = (MenuComponent)iterator.next();
+			component.print();
+		}
+	}
+}
