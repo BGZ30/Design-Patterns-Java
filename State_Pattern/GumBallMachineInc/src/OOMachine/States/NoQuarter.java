186fc780a27aa9f5d11e5627fda81bfea45b8aa1
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-06-09 14:49:00 +0200

    NoQuarter state class

--- State_Pattern/GumBallMachineInc/src/OOMachine/States/NoQuarter.java
+++ State_Pattern/GumBallMachineInc/src/OOMachine/States/NoQuarter.java
@@ -12,25 +12,21 @@ public class NoQuarter implements State {
 	public void insertQuarter() {
 		System.out.println("You inserted a quarter");
 		gumballMachine.setState(gumballMachine.getHasQuarterState());
-
 	}
 
 	@Override
 	public void ejectQuarter() {
 		System.out.println("You haven’t inserted a quarter");
-
 	}
 
 	@Override
 	public void turnCrank() {
 		System.out.println("You turned, but there’s no quarter");
-
 	}
 
 	@Override
 	public void despense() {
 		System.out.println("You need to pay first");
-
 	}
 
 }
