7ebdadd868f17f2b3b120ad2cca67a7bfc1c2421
bgz30 <44124521+BGZ30@users.noreply.github.com>
2020-06-09 15:07:13 +0200

    SoldOut state class

---
+++ State_Pattern/GumBallMachineInc/src/OOMachine/States/SoldOut.java
@@ -0,0 +1,31 @@
+package OOMachine.States;
+
+public class SoldOut implements State {
+
+	GumballMachine gumballMachine;
+	
+	public SoldOut(GumballMachine gumballMachine) {
+		this.gumballMachine = gumballMachine;
+	}
+
+	@Override
+	public void insertQuarter() {
+		System.out.println("You can’t insert a quarter, the machine is sold out");
+	}
+
+	@Override
+	public void ejectQuarter() {
+		System.out.println("You can’t eject, you haven’t inserted a quarter yet");
+	}
+
+	@Override
+	public void turnCrank() {
+		System.out.println("You turned, but there are no gumballs");
+	}
+
+	@Override
+	public void despense() {
+		System.out.println("No gumball dispensed");
+	}
+
+}
